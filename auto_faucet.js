ddcc3487007c8ba3c4049bf4b5fd0a4d7c95229d
masbad123 <winterrandom76@gmail.com>
2025-02-23 18:53:55 +0800

    Update auto_faucet.js

--- auto_faucet.js
+++ auto_faucet.js
@@ -1,31 +1,29 @@
-const axios = require('axios');
+const puppeteer = require('puppeteer');
 
-// URL dari faucet API yang ditemukan dari alat pengembang
-const faucetUrl = 'https://cloud.google.com/application/web3/_/Web3Portal/data/batchexecute?rpcids=lMZlm&source-path=%2Fapplication%2Fweb3%2Ffaucet%2Fethereum%2Fsepolia&bl=boq_cloud-web3-portal-web-ui_20250220.07_p0&f.sid=7826119320332474033&hl=en-US&_reqid=263784&rt=c'; 
+const faucetUrl = 'https://cloud.google.com/application/web3/faucet/ethereum/sepolia';
+const walletAddress = 'your_wallet_address_here';
+const recaptchaToken = 'your_recaptcha_token_here'; // Ganti dengan token reCAPTCHA yang valid
 
-// Data yang dikirim dalam permintaan POST
-const postData = {
-    // Sesuaikan dengan payload yang ditemukan dari alat pengembang
-    "wallet_address": "your_wallet_address_here"
-};
+(async () => {
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
 
-async function claimFaucet() {
-    try {
-        const response = await axios.post(faucetUrl, postData, {
-            headers: {
-                'Content-Type': 'application/json',
-                // Tambahkan header lain yang diperlukan
-            }
-        });
+    await page.goto(faucetUrl);
 
-        if (response.status === 200) {
-            console.log('Faucet claim successful:', response.data);
-        } else {
-            console.log('Failed to claim faucet:', response.status, response.statusText);
-        }
-    } catch (error) {
-        console.error('Error claiming faucet:', error.message);
-    }
-}
+    // Isi alamat wallet address
+    await page.type('#walletAddress', walletAddress);
 
-claimFaucet();
+    // Isi token reCAPTCHA
+    await page.evaluate((token) => {
+        document.querySelector('#recaptcha-token').value = token;
+    }, recaptchaToken);
+
+    // Klik tombol klaim faucet
+    await page.click('#claimButton');
+
+    // Tunggu beberapa detik untuk memastikan permintaan diproses
+    await page.waitForTimeout(5000);
+
+    await browser.close();
+    console.log('Faucet claim successful');
+})();
